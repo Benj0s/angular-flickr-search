631b89ac5b8e2519c1c0147df9ec41432451ad11
Benj0s <benpearey@hotmail.co.uk>
2015-01-11 12:30:38 +0000

    New styles and load more button tweaks

--- public/app/components/search/search.controllers.js
+++ public/app/components/search/search.controllers.js
@@ -6,13 +6,35 @@
 
 angular.module('flickrSearchApp').controller('SearchController', ['$scope', 'Search',
     function($scope, Search) {
-        $scope.photos = {};
+        $scope.photos = [];
         $scope.searchString = '';
+        $scope.currentPage = 1;
+        $scope.maxPages = 1;
+        $scope.hasMore = false;
+
+        $scope.search = function() {
+            $scope.currentPage = 1;
+            $scope.maxPages = 1;
+            $scope.getPhotos();
+        };
 
         $scope.getPhotos = function() {
-            Search.query({searchString: $scope.searchString}, function(data){
-                $scope.photos = data.photos;
+            Search.query({searchString: $scope.searchString, pageNum: $scope.currentPage}, function(data){
+                $scope.photos.push.apply($scope.photos, data.photos.photo);
+                $scope.maxPages = data.photos.pages;
+                $scope.hasMorePages();
             });
         };
+
+        $scope.getNextPage = function() {
+            $scope.currentPage += 1;
+            $scope.hasMorePages();
+            $scope.getPhotos();
+        };
+
+        $scope.hasMorePages = function() {
+            $scope.hasMore = $scope.currentPage < $scope.maxPages ? true : false;
+        };
+
     }
 ]);
--- public/app/components/search/search.services.js
+++ public/app/components/search/search.services.js
@@ -10,8 +10,8 @@ var flickrSearchServices = angular.module('flickrSearchServices', ['ngResource']
 
 flickrSearchServices.factory('Search', ['$resource',
     function($resource) {
-        return $resource('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8abf75ed68b16cf1e1ac1f0cb1cf9527&text=:searchString&extras=url_sq%2C+url_t%2C+url_s%2C+url_q%2C+url_m%2C+url_n%2C+url_z%2C+url_c%2C+url_l%2C+url_o&format=json&nojsoncallback=1', {}, {
-            query: {method: 'GET', params: {searchString: 'ballons'}, isArray: false}
+        return $resource('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8abf75ed68b16cf1e1ac1f0cb1cf9527&text=:searchString&page=:pageNum&extras=url_sq%2C+url_t%2C+url_s%2C+url_q%2C+url_m%2C+url_n%2C+url_z%2C+url_c%2C+url_l%2C+url_o&format=json&nojsoncallback=1', {}, {
+            query: {method: 'GET', params: {searchString: 'test', pageNum: 1}, isArray: false}
         });
     }
 ]);
